039e319bdd020e8ccdde774f90248da8665007eb
foxwel88 <cttony1997@126.com>
2018-06-02 15:11:27 +0800

    finish dao module

---
+++ API_Stack/src/main/java/com/edu/nju/dao/Dao.java
@@ -0,0 +1,92 @@
+package com.edu.nju.dao;
+
+import com.edu.nju.model.Example;
+import com.edu.nju.model.Method;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class Dao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+
+    public void test() {
+        Session session = getSession();
+        session.beginTransaction();
+        Method method = getSession().load(Method.class, 1);
+        session.close();
+        System.out.println(method.getName());
+    }
+
+    //通过库名(fs/mongodb/lodash)获得所有类列表
+    public List<String> getClassNameList(String libName) {
+        Session session = getSession();
+        session.beginTransaction();
+
+        String hql = "SELECT DISTINCT m.method_class FROM Method m where m.lib_name LIKE '%" + libName + "%'";
+
+        Query query = session.createQuery(hql);
+        List<String> res = query.list();
+        session.close();
+        return res;
+    }
+
+    //通过库名(fs/mongodb/lodash)和类名获得该类所有方法列表
+    public List<Method> getMethodList(String libName, String className) {
+        Session session = getSession();
+        session.beginTransaction();
+
+        String hql = "FROM Method m where m.lib_name LIKE '%" + libName + "%' AND m.method_class ='" + className + "'";
+
+        Query query = session.createQuery(hql);
+
+        List<Method> res = query.list();
+        session.close();
+        return res;
+    }
+
+    //通过库名(fs/mongodb/lodash)获得所有方法列表
+    public List<Method> getMethodList(String libName) {
+        Session session = getSession();
+        session.beginTransaction();
+
+        String hql = "FROM Method m where m.lib_name LIKE '%" + libName + "%'";
+
+        Query query = session.createQuery(hql);
+
+        List<Method> res = query.list();
+        session.close();
+        return res;
+    }
+
+    //通过方法ID获得Example列表
+    public List<Example> getExample(int methodID) {
+        Session session = getSession();
+        session.beginTransaction();
+
+        String hql = "FROM Example e where e.mid = " + methodID + "ORDER BY e.score DESC";
+
+        Query query = session.createQuery(hql);
+
+        List<Example> res = query.list();
+        session.close();
+        return res;
+    }
+
+}
---
+++ API_Stack/src/main/java/com/edu/nju/model/Example.java
@@ -0,0 +1,105 @@
+package com.edu.nju.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="so_example")
+public class Example {
+
+    @Id
+    private int id;
+
+    //方法id
+    private int mid;
+
+    //所属stackoverflow回答id
+    private int postId;
+
+    //原始代码(不用展示)
+    private String code;
+
+    //回答内容
+    private String body;
+
+    //回答分数
+    private int score;
+
+    //所属问题id
+    private int parentId;
+
+    //所有代码
+    private String[] codeList;
+
+    public Example() {
+    }
+
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public String[] getCodeList() {
+        codeList = code.split("\\(20080808\\)");
+        return codeList;
+    }
+
+    public void setCodeList(String[] codeList) {
+        this.codeList = codeList;
+    }
+}
